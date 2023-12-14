6131cb13a25de983306d3648f328de11e49e9784
Raden Muhammad Farrel Arrahman <arrahman_farrel@yahoo.com>
2023-12-14 23:56:42 +0800

    vite

--- resources/views/layout/sidebar.blade.php
+++ resources/views/layout/sidebar.blade.php
@@ -157,7 +157,7 @@
                         </ul>
                     </div>
                 </li>
-                <li class="nav-item ">
+                <!-- <li class="nav-item ">
                     <a href="{{ route('settings.index') }}" class="nav-link">
                         <span class="sidebar-icon">
                             <svg class="icon icon-xs me-2" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
@@ -166,10 +166,10 @@
                         </span>
                         <span class="sidebar-text">Pengaturan</span>
                     </a>
-                </li>
+                </li> -->
                 @endif
                 <li class="nav-item">
-                    <a href="#" class="btn btn-danger d-flex align-items-center justify-content-center btn-upgrade-pro">
+                    <a onclick="$('#logout-form').submit()" href="#" class="btn btn-danger d-flex align-items-center justify-content-center btn-upgrade-pro">
                         <span class="sidebar-icon d-inline-flex align-items-center justify-content-center">
                             <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-box-arrow-right" viewBox="0 0 16 16">
                                 <path fill-rule="evenodd" d="M10 12.5a.5.5 0 0 1-.5.5h-8a.5.5 0 0 1-.5-.5v-9a.5.5 0 0 1 .5-.5h8a.5.5 0 0 1 .5.5v2a.5.5 0 0 0 1 0v-2A1.5 1.5 0 0 0 9.5 2h-8A1.5 1.5 0 0 0 0 3.5v9A1.5 1.5 0 0 0 1.5 14h8a1.5 1.5 0 0 0 1.5-1.5v-2a.5.5 0 0 0-1 0v2z"/>
